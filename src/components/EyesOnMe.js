c58872603c89aba3d54efc85a19b3d52af23417d
Fawzi Gharib <fawz11gharib@gmail.com>
2024-03-18 02:14:41 +0300

    Assignment Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+
+
+function EyesOnMe() {
+  return (
+    <div>
+      <button
+        onFocus={(e) => {
+          console.log('Good!');
+        }}
+
+        onBlur={(e) => {
+          console.log('Hey! Eyes on me!');
+        }}
+        placeholder="onFocus is triggered when you click this input."
+      >Eyes on me</button>
+    </div>
+  )
+}
+
+
+export default EyesOnMe;
